f3e6e3f90d99fec27dcb51c82a90446383c649a3
tuananh279 <tuananhptk4@gmail.com>
2022-01-21 15:03:19 +0700

    add lib-jitsi-meet

---
+++ service/RTC/VideoType.js
@@ -0,0 +1,17 @@
+/**
+ * Enumeration of the video types
+ * @type {{CAMERA: string, DESKTOP: string}}
+ */
+const VideoType = {
+    /**
+     * The camera video type.
+     */
+    CAMERA: 'camera',
+
+    /**
+     * The desktop video type.
+     */
+    DESKTOP: 'desktop'
+};
+
+module.exports = VideoType;
